b8bd55124cfb18a7263b3c2bfa45682799ac49be
BriWins <95152254+BriWins@users.noreply.github.com>
2022-08-10 10:28:01 -0500

    registration complete

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/internal/operators';
+import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -17,7 +17,7 @@ export class FetchApiDataService {
  // Making the api call for the user registration endpoint
   public userRegistration(userDetails: any): Observable<any> {
     console.log(userDetails);
-    return this.http.post(apiUrl + 'users', userDetails).pipe(
+    return this.http.post(apiUrl + `users`, userDetails).pipe(
     catchError(this.handleError)
     );
   }
@@ -25,7 +25,7 @@ export class FetchApiDataService {
   // Making the api call for the user login endpoint
   public userLogin(userCredentials: any): Observable<any> {
     console.log(userCredentials);
-    return this.http.post(apiUrl + 'login', userCredentials).pipe(
+    return this.http.post(apiUrl + `login`, userCredentials).pipe(
     catchError(this.handleError)
     );
   }
@@ -33,7 +33,7 @@ export class FetchApiDataService {
 // Making the api call for retrieving all movies
   public getAllMovies(allMovies: any): Observable<any> {
     console.log(allMovies);
-    return this.http.get(apiUrl + 'movies', allMovies).pipe(
+    return this.http.get(apiUrl + `movies`, allMovies).pipe(
     catchError(this.handleError)
     );
   }
@@ -41,7 +41,7 @@ export class FetchApiDataService {
 // Making the api call for retrieving a single movie by title
   public getSingleMovie(singleMovie: any): Observable<any> {
     console.log(singleMovie);
-    return this.http.get(apiUrl + 'movies/${title}', singleMovie).pipe(
+    return this.http.get(apiUrl + `movies/title`, singleMovie).pipe(
     catchError(this.handleError)
     );
   }
@@ -49,7 +49,7 @@ export class FetchApiDataService {
 // Making the api call for getting info on a director
   public movieDirector(directorInfo: any): Observable<any> {
     console.log(directorInfo);
-    return this.http.get(apiUrl + 'movies/director/${name}', directorInfo).pipe(
+    return this.http.get(apiUrl + `movies/director/name`, directorInfo).pipe(
     catchError(this.handleError)
     );
   }
@@ -57,7 +57,7 @@ export class FetchApiDataService {
 // Making the api call for getting info on a genre
   public movieGenre(genreInfo: any): Observable<any> {
     console.log(genreInfo);
-    return this.http.get(apiUrl + 'movies/genre/${name}', genreInfo).pipe(
+    return this.http.get(apiUrl + `movies/genre/name`, genreInfo).pipe(
     catchError(this.handleError)
     );
   }
@@ -65,7 +65,7 @@ export class FetchApiDataService {
 // Making the api call to get user information
   public getUser(userInfo: any): Observable<any> {
     console.log(userInfo);
-    return this.http.get(apiUrl + 'user/${username}', userInfo).pipe(
+    return this.http.get(apiUrl + `user/username`, userInfo).pipe(
     catchError(this.handleError)
     );
   }
@@ -73,7 +73,7 @@ export class FetchApiDataService {
 // Making the api call to add a movies to favorites list
   public addMovie(addFavorite: any): Observable<any> {
     console.log(addFavorite);
-    return this.http.post(apiUrl + 'user/favorites', addFavorite).pipe(
+    return this.http.post(apiUrl + `user/favorites`, addFavorite).pipe(
     catchError(this.handleError)
     );
   }
@@ -81,7 +81,7 @@ export class FetchApiDataService {
 // Making the api call to remove a movie by title from favorites list
   public deleteMovie(removeFavorite: any): Observable<any> {
     console.log(removeFavorite);
-    return this.http.delete(apiUrl + 'user/favorites/${title}', removeFavorite).pipe(
+    return this.http.delete(apiUrl + `user/favorites/title`, removeFavorite).pipe(
     catchError(this.handleError)
     );
   }
@@ -89,7 +89,7 @@ export class FetchApiDataService {
 // Making the api call to update user profile
   public updateUser(updateUser: any): Observable<any> {
     console.log(updateUser);
-    return this.http.put(apiUrl + 'user/${username}', updateUser).pipe(
+    return this.http.put(apiUrl + `user/username`, updateUser).pipe(
     catchError(this.handleError)
     );
   }
@@ -97,7 +97,7 @@ export class FetchApiDataService {
 // Making the api call to delete user profile
   public deleteAcct (deleteUser: any): Observable<any> {
     console.log(deleteUser);
-    return this.http.delete(apiUrl + 'user/${username}', deleteUser).pipe(
+    return this.http.delete(apiUrl + `user/username`, deleteUser).pipe(
     catchError(this.handleError)
     );
   }
