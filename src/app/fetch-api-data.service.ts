d093786f7514d7156789382100fa196ebce19e47
BriWins <95152254+BriWins@users.noreply.github.com>
2022-08-15 21:05:47 -0500

    correct heroku url

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -5,7 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 //Declaring the api url that will provide data for the client app
-const apiUrl = 'https://git.heroku.com/stark-oasis-54313/';
+const apiUrl = 'https://stark-oasis-54313.herokuapp.com/';
 
 @Injectable({
   providedIn: 'root'
