5d759895edc8f6c059c4264de8a2324a0078810b
BriWins <95152254+BriWins@users.noreply.github.com>
2022-08-17 10:31:50 -0500

    update parameters

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -18,7 +18,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  @Input() userData = { Username: '', Password: '', Email: '', Birthdate: '' };
+  @Input() userData = { username: '', password: '', email: '', birthdate: '' };
 
 constructor(
     public fetchApiData: FetchApiDataService,
